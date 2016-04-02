eeb1015dd130eb74b56e3cc31261fb435d482b60
Iamtodor <todor.ilya@gmail.com>
2016-04-02 16:40:28 +0300

    Some small fixes and make code more clear

--- app/src/main/java/com/todor/kedditbysteps/NewsFragment.kt
+++ app/src/main/java/com/todor/kedditbysteps/NewsFragment.kt
@@ -3,27 +3,26 @@ package com.todor.kedditbysteps
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.todor.kedditbysteps.commons.inflate
 import kotlinx.android.synthetic.main.news_fragment.*
 
 class NewsFragment : Fragment() {
 
-    private var newsList: RecyclerView? = null
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
-
-        return view
+    private val newsList by lazy {
+        news_list
     }
 
-    fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
-        return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        newsList?.setHasFixedSize(true)
+        newsList?.layoutManager = LinearLayoutManager(context)
     }
 }
 
